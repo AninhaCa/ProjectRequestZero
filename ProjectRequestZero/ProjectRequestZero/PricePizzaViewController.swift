7c8c73f9a231ac6efcd2df281a607515f2564803
Aninha <aninhapss2000@gmail.com>
2023-03-15 19:47:42 -0300

    Added PricePizzaViewController, RatingPizzaViewController

---
+++ ProjectRequestZero/ProjectRequestZero/PricePizzaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TwoViewController.swift
+//  ProjectRequestZero
+//
+//  Created by Ana Paula Silva de Sousa on 15/03/23.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
