0cdb4f2cf759bd49434b2912f19e19b19228a49e
Aninha <aninhapss2000@gmail.com>
2023-03-14 21:21:42 -0300

    Added extension dataSource

--- ProjectRequestZero/ProjectRequestZero/ViewController.swift
+++ ProjectRequestZero/ProjectRequestZero/ViewController.swift
@@ -13,9 +13,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cellXIB", for: indexPath) as? MyCustomCellXIB {
+            
+             return cell
+        }
+        
+        return UITableViewCell()
+    }
+}
