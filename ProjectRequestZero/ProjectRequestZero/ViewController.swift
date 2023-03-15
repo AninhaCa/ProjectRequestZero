51316bd7cdeffa37e0f7b3445a65337e75c2c22f
Aninha <aninhapss2000@gmail.com>
2023-03-14 22:04:17 -0300

    Added debugPrint(response)

--- ProjectRequestZero/ProjectRequestZero/ViewController.swift
+++ ProjectRequestZero/ProjectRequestZero/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
 
     func requestPizza() {
         AF.request("https://p3teufi0k9.execute-api.us-east-1.amazonaws.com/v1/pizza", method: .get).response { response in
-            print(response)
+            debugPrint(response)
         }
     }
 }
